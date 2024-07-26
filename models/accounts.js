0eb148b9737efd0863d1da7f3d78072044e3fc4d
Michael Chan <michael.w.chan01@gmail.com>
2024-07-25 22:09:02 -0400

    Created account schema

--- models/accounts.js
+++ models/accounts.js
@@ -1,5 +1,24 @@
 const mongoose = require('mongoose');
 
 const AccountSchema = new mongoose.Schema({
-    
+    //Ser user to type in users.js
+    user: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User'
+    },
+    accountNumber: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    balance: {
+        type: Number,
+        required: true,
+    },
+    accountType:{
+        type: String,
+        required: true
+    }
 })
+
+module.exports = mongoose.model('Account', AccountSchema);
