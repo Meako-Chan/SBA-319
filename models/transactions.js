c359654956bdf24139020671217b449c78d736f6
Michael Chan <michael.w.chan01@gmail.com>
2024-07-25 22:37:01 -0400

    Transaction schema model

--- models/transactions.js
+++ models/transactions.js
@@ -1,5 +1,23 @@
 const mongoose = require('mongoose');
 
-const AccountSchema = new mongoose.Schema({
-    
-})
+const TransactionSchema = new mongoose.Schema({
+    account: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Account',
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    transactionType: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Transaction', TransactionSchema);
